92b9084cedb27c0af14e1559bbac3d689368e0f3
Josh Clark <jclarktennis@gmail.com>
2025-12-16 15:37:59 -0500

    chore: run new strategies

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from box_state import BoxState
 from data_visualization import show_all_visualizations
 from dice import Dice
 from game import Game
-from strategy import WinPercentMostNumbersHighStrategy, WinPercentOptimalStrategy
+from strategy import ScoreOptimalStrategy, WinPercentHighestNumberStrategy, WinPercentMostNumbersHighStrategy, WinPercentMostNumbersLowStrategy, WinPercentOptimalStrategy
 
 
 def main():
@@ -12,20 +12,25 @@ def main():
     # starting with 1
     starting_game_state = BoxState([num for num in range(1, MAX_NUMBER_IN_BOX + 1)])
     dice = Dice([6, 6])
-    strategy = WinPercentOptimalStrategy()
-    game = Game(
-        starting_game_state,
-        dice,
-        strategy
-    )
+    strategies = [
+        WinPercentOptimalStrategy(),
+        # WinPercentMostNumbersHighStrategy(),
+        # WinPercentMostNumbersLowStrategy(),
+        # WinPercentHighestNumberStrategy(),
+    ]
 
-    # Play
-    game.solve_all_box_states()
+    for strategy in strategies:
+        game = Game(
+            starting_game_state,
+            dice,
+            strategy
+        )
 
-    # Print
-    print("Strategy:", str(strategy))
-    print("Expectation with above strategy:", game.all_box_states[starting_game_state.id].expectation)
-    show_all_visualizations(game)
+        game.solve_all_box_states()
+        print(f"{strategy}: {game.all_box_states[starting_game_state.id].expectation}")
+
+    # show_all_visualizations(game)
+    print("")
 
 if __name__ == "__main__":
     main()
