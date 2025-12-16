782d9443c0ff5d5926b6d57eb04e3efe07a43c04
Josh Clark <jclarktennis@gmail.com>
2025-12-15 21:00:26 -0500

    feat: add 'win percent most numbers/high' strategy

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from box_state import BoxState
 from dice import Dice
 from game import Game
-from strategy import WinPercentOptimalStrategy
+from strategy import WinPercentMostNumbersHighStrategy, WinPercentOptimalStrategy
 
 
 def main():
-    starting_game_state = BoxState([num for num in range(1, 10)])
+    # Set up
+    starting_game_state = BoxState([num for num in range(1, 13)])
     dice = Dice([6, 6])
-    strategy = WinPercentOptimalStrategy
+    # strategy = WinPercentOptimalStrategy()
+    strategy = WinPercentMostNumbersHighStrategy()
     print(dice.distribution)
-    win_percent_game = Game(
+    game = Game(
         starting_game_state,
         dice,
         strategy
     )
 
-    win_percent_game.solve_all_box_states()
-    print("Done!")
+    # Play
+    game.solve_all_box_states()
+
+    # Print
+    print("Strategy:", str(strategy))
+    print("Expectation with above strategy:", game.all_box_states[starting_game_state.id].expectation)
 
 if __name__ == "__main__":
     main()
--- strategy.py
+++ strategy.py
@@ -14,20 +14,29 @@ class Strategy(ABC):
     @abstractmethod
     def get_game_over_box_state_value(box_state: BoxState) -> int:
         """Determine the value of the given box state if the game is over."""
-        pass
+        raise NotImplementedError
 
     @staticmethod
     @abstractmethod
     def pick_next_box_state_from_options(box_states: list[BoxState]) -> BoxState:
         """Pick the next game state from the given options."""
-        pass
+        raise NotImplementedError
 
 
-class WinPercentOptimalStrategy(Strategy):
+class WinPercentStrategy(Strategy):
     @staticmethod
-    def get_game_over_box_state_value(box_state: BoxState) -> int:
+    def get_game_over_box_state_value(box_state: BoxState):
         return 1 if len(box_state.numbers) == 0 else 0
-    
+
+    @staticmethod
+    def pick_next_box_state_from_options(box_states: list[BoxState]) -> BoxState:
+        return super().pick_next_box_state_from_options(box_states)
+
+
+class WinPercentOptimalStrategy(WinPercentStrategy):
+    def __str__(self):
+        return "Win % optimal strategy"
+
     @staticmethod
     def pick_next_box_state_from_options(box_states: list[BoxState]) -> BoxState:
         best_option = None
@@ -35,3 +44,17 @@ class WinPercentOptimalStrategy(Strategy):
             if not best_option or box_state.expectation > best_option.expectation:
                 best_option = box_state
         return best_option
+
+
+class WinPercentMostNumbersHighStrategy(WinPercentStrategy):
+    def __str__(self):
+        return "Win % most numbers high strategy"
+
+    @staticmethod
+    def pick_next_box_state_from_options(box_states: list[BoxState]) -> BoxState:
+        """Pick the state that removes the most numbers, or the one that removes the highest number if tied."""
+        sorted_by_num_of_nums = sorted(
+            box_states,
+            key=lambda state: (len(state.numbers), max(state.numbers) if len(state.numbers) > 0 else 0)
+        )
+        return sorted_by_num_of_nums[0]
